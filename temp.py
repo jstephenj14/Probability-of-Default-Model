db7c97cb0c09ca6a6ee96f55b7a7a252017c0ea2
aruls001 <aruls001@umn.edu>
2018-03-10 17:03:23 -0600

    Missing Value Treatment Complete

--- temp.py
+++ temp.py
@@ -6,6 +6,11 @@ import seaborn as sns
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import graphviz
 import missingno as msno
+from sklearn import tree
+import scipy.stats as stats
+import warnings
+warnings.filterwarnings("ignore")
+
 
 os.chdir('C:\\Users\\jstep\\Downloads\\Credit Risk Analytics Pack\\Probablity of Default\\Supplements')
 PD_data = pd.read_excel("PD.xls")
@@ -16,8 +21,67 @@ PD_data.housing1.value_counts()
 
 PD_data = PD_data.reset_index()
 
-### Missing Value Check
+######FILTER MISSING OUT
 
+PD_data_nm = PD_data[PD_data["housing1"] != "."]
+
+PD_data_nm["housing1"] = pd.to_numeric(PD_data_nm["housing1"])
+
+features = np.asarray(PD_data.columns[(PD_data.columns != "goodbad")&
+                                      (PD_data.columns != "housing1")&
+                                      (PD_data.columns != "is_missing")&
+                                      (PD_data.columns != "index")])
+
+X = PD_data_nm[features]
+y = PD_data_nm["housing1"]
+
+msk = np.random.rand(len(X)) < 0.8
+train_X = X[msk]
+test_X = X[~msk]
+train_y = y[msk]
+test_y = y[~msk]
+
+dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
+dt = dt.fit(train_X,train_y)
+
+PD_train_nm = PD_data_nm[msk]
+PD_test_nm = PD_data_nm[~msk]
+
+PD_train_nm["predicted"] = dt.predict(train_X)
+pd.crosstab(PD_train_nm["predicted"],PD_train_nm["housing1"] )
+
+PD_test_nm["predicted"] = dt.predict(test_X) 
+pd.crosstab(PD_test_nm["predicted"],PD_test_nm["housing1"] )
+# Excellent accuracy!
+
+# Fit model to missing data
+PD_data_m = PD_data[PD_data["housing1"] == "."]
+
+PD_data_m["housing1"] = dt.predict(PD_data_m[features])
+
+# Append to non-missing dataset
+
+PD_final = PD_data_nm.append(PD_data_m)
+
+# Now PD final is ready. 
+# Build WOE buckets
+
+duration_WOE = WOE_Binning("goodbad",PD_final[["goodbad","duration"]],sign = False)
+age_WOE = WOE_Binning("goodbad",PD_final[["goodbad","age"]],sign = True)
+history1_WOE = WOE_Binning("goodbad",PD_final[["goodbad","history1"]], sign = True)
+
+
+
+
+
+
+
+
+
+
+
+
+######################CHECKING MISSING ITSELF
 def func(row):
     if row["housing1"] == ".":
         return 1
@@ -27,23 +91,46 @@ def func(row):
 PD_data["is_missing"] = PD_data.apply(func,axis = 1)
 
 y = PD_data["is_missing"]
-features = np.asarray(PD_data.columns[(PD_data.columns != "goodbad")&(PD_data.columns != "housing1")])
+features = np.asarray(PD_data.columns[(PD_data.columns != "goodbad")&
+                                      (PD_data.columns != "housing1")&
+                                      (PD_data.columns != "is_missing")&
+                                      (PD_data.columns != "index")])
 X = PD_data[features]
 
+msk = np.random.rand(len(X)) < 0.8
+train_X = X[msk]
+test_X = X[~msk]
+train_y = y[msk]
+test_y = y[~msk]
+
 dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-dt.fit(X, y)
+dt = dt.fit(train_X,train_y)
 
-from sklearn.externals.six import StringIO  
-from IPython.display import Image
-import pydotplus
+y_predict = dt.predict(train_X)
 
-dot_data = StringIO()
-export_graphviz(dt, out_file=dot_data,  
-                filled=True, rounded=True,
-                special_characters=True)
+PD_train = PD_data[msk]
 
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-Image(graph.create_png())
+PD_train["predicted"] = dt.predict(train_X)
+
+with open("dt_classifier.txt", "w") as f:
+    f = tree.export_graphviz(dt, out_file=f)
+## Then take this file to http://webgraphviz.com/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 PD_melted = pd.melt(PD_data,id_vars=["index","goodbad"])
